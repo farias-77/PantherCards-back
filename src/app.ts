f5d2f758b794bb0acc666b7431801f024478a9b0
farias-77 <gabrielfalves2003@gmail.com>
2022-09-20 09:08:39 -0300

    feat: sign-up and sign-in

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import "express-async-errors";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import errorHandler from "./middlewares/errorHandlingMiddleware";
+import router from "./routers/indexRouter";
+
 const app = express();
 app.use(cors());
 app.use(json());
 dotenv.config();
+
+app.use(router);
+app.use(errorHandler)
 
 const PORT: number = Number(process.env.PORT);
 app.listen(PORT, () => {
