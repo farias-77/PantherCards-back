204cb33f2cff59dc3f603bedc1c3baf1dac0c333
farias-77 <gabrielfalves2003@gmail.com>
2022-09-20 08:30:52 -0300

    feat: setup node project

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(json());
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+})
