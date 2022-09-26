b5731b225e441f1242219cc38f27f5714c53ea7e
farias-77 <gabrielfalves2003@gmail.com>
2022-09-26 14:37:22 -0300

    fix: adds username to user insertion routine

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,21 +4,25 @@ import { TUser } from "../types/userTypes";
 import * as authServices from "../services/authServices";
 
 export async function signUp(req: Request, res: Response) {
-  const user: TUser = { email: req.body.email, password: req.body.password };
-  const confirmPassword: string = req.body.confirmPassword;
+    const user: TUser = {
+        email: req.body.email,
+        password: req.body.password,
+        username: req.body.username,
+    };
+    const confirmPassword: string = req.body.confirmPassword;
 
-  authServices.validateConfirmPassword(user.password, confirmPassword);
-  await authServices.validateNewEmail(user.email);
-  const createdUser = await authServices.insertUser(user);
+    authServices.validateConfirmPassword(user.password, confirmPassword);
+    await authServices.validateNewEmail(user.email);
+    const createdUser = await authServices.insertUser(user);
 
-  res.status(201).send(createdUser);
+    res.status(201).send(createdUser);
 }
 
 export async function signIn(req: Request, res: Response) {
-  const user: TUser = req.body;
+    const user: TUser = req.body;
 
-  await authServices.validatePassword(user);
-  const token: string = await authServices.generateToken(user.email);
+    await authServices.validatePassword(user);
+    const token: string = await authServices.generateToken(user.email);
 
-  res.status(200).send({ token });
+    res.status(200).send({ token });
 }
--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -3,11 +3,12 @@ import { TUser } from "../types/userTypes";
 
 export const signUpSchema = joi.object({
     email: joi.string().email().required(),
+    username: joi.string().required(),
     password: joi.string().required(),
-    confirmPassword: joi.string().required()
+    confirmPassword: joi.string().required(),
 });
 
 export const signInSchema = joi.object<TUser>({
     email: joi.string().email().required(),
-    password: joi.string().required()
+    password: joi.string().required(),
 });
