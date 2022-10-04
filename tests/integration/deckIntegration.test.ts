cca322173c52d1e19f414d1054680bd83abaf300
farias-77 <gabrielfalves2003@gmail.com>
2022-10-04 13:25:40 -0300

    feat: tests for questions insertion

--- tests/integration/deckIntegration.test.ts
+++ tests/integration/deckIntegration.test.ts
@@ -1,6 +1,6 @@
 import supertest from "supertest";
 import app from "../../src/app";
-import { deckFactory } from "../factories/deckFactory";
+import { deckFactory, questionsFactory } from "../factories/deckFactory";
 import { tokenFactory, userFactory } from "../factories/userFactory";
 
 const server = supertest(app);
@@ -40,5 +40,29 @@ describe("Testa /POST em /deck", () => {
             .send(deck);
 
         expect(result.status).toBe(401);
+    });
+});
+
+describe("Testa /POST em /deck/questions/:deckId", () => {
+    it("Testa com perguntas válidas", async () => {
+        const deck = deckFactory();
+        const user = userFactory();
+        const questions = questionsFactory();
+
+        const { body: signUp } = await server.post("/sign-up").send(user);
+        const token = await tokenFactory(signUp.id);
+
+        const { body: createdDeck } = await server
+            .post("/deck")
+            .set("Authorization", `Bearer ${token}`)
+            .send(deck);
+
+        const result = await server
+            .post(`/deck/questions/${createdDeck.id}`)
+            .set("Authorization", `Bearer ${token}`)
+            .send(questions);
+
+        expect(result.status).toBe(201);
+        expect(result.body).toBeInstanceOf(Object);
     });
 });
